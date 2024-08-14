2f0c48ac4a9149180334b4e148c717a1686059df
Sanjeelathapa7 <94773938+Sanjeelathapa7@users.noreply.github.com>
2024-08-14 13:20:00 +0545

    update

--- src/pages/user/Favorite.jsx
+++ src/pages/user/Favorite.jsx
@@ -7,6 +7,7 @@ import { getFavoritesApi, removeFavoriteApi } from "../../apis/Apis";
 const AddToFavorites = ({}) => {
   const [favorites, setFavorites] = useState([]);
 
+  
   useEffect(() => {
     const storedUserData = localStorage.getItem("user");
     const parsedUserData = JSON.parse(storedUserData);
